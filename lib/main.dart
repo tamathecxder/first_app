0c6b8117069dd0783c6ae9117fac0a269ba364a3
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-09-26 21:28:09 +0700

    Memberi colors dan font styling serta mengubah default font family

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,24 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(
           title: Text("Weather App"),
           centerTitle: false,
+          backgroundColor: Colors.deepPurple[600],
         ),
         body: Center(
-          child: Text("Hello, flutter!"),
+          child: Text(
+            "Hello, Flutter!",
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[600],
+              letterSpacing: 2.0,
+              fontFamily: "IndieFlower",
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: null,
           child: Text("Click"),
+          backgroundColor: Colors.deepPurple[600],
         ),
       ),
     ));
