ebce199d315e0aca37b11f3c5c90480685800b1e
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-09-26 21:00:21 +0700

    Struktur apps sederhana dengan beberapa widgets yang dipasangkan

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Weather App"),
+          centerTitle: false,
+        ),
+        body: Center(
+          child: Text("Hello, flutter!"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Text("Click"),
+        ),
+      ),
+    ));
