af68afe5d3a1ac81ad6062e735950ffcb076cfa2
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-09-28 13:19:32 +0700

    Mengubah struktur kode kedalam sebuah stateless widget agar hot reload dapat berjalan dengan baik.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Weather App"),
-          centerTitle: false,
-          backgroundColor: Colors.deepPurple[600],
-        ),
-        body: Center(
-          child: Text(
-            "Hello, Flutter!",
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.grey[600],
-              letterSpacing: 2.0,
-              fontFamily: "IndieFlower",
-            ),
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Clock Application"),
+        centerTitle: false,
+        backgroundColor: Colors.deepPurple[600],
+      ),
+      body: Center(
+        child: Text(
+          "Hello World!",
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[600],
+            letterSpacing: 2.0,
+            fontFamily: "IndieFlower",
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Text("Click"),
-          backgroundColor: Colors.deepPurple[600],
-        ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Text("Click"),
+        backgroundColor: Colors.deepPurple[600],
+      ),
+    );
+  }
+}
