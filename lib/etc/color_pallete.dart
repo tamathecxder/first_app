0e4553ddc2e67c57733ebe50bfb0e8c48fdada8c
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-10-22 18:36:35 +0700

    Icon dan button widgets pada flutter

---
+++ lib/etc/color_pallete.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorPallete {
+  static const Color darkPrimary = Color.fromRGBO(48, 56, 65, 1);
+  static const Color semiDarkPrimary = Color.fromRGBO(58, 71, 80, 1);
+  static const Color mainBrightYellow = Color.fromRGBO(246, 201, 14, 1);
+  static const Color greySecondary = Color.fromRGBO(238, 238, 238, 1);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:beginner_app/etc/color_pallete.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: Home()));
@@ -8,15 +9,24 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorPallete.darkPrimary,
       appBar: AppBar(
         title: Text("Clocks"),
-        centerTitle: false,
-        backgroundColor: Colors.yellow[800],
+        centerTitle: true,
+        backgroundColor: ColorPallete.semiDarkPrimary,
       ),
       body: Center(
-        child: Image(
-          // image: NetworkImage("https://picsum.photos/1000/1000"),
-          image: AssetImage("assets/pic1.jpg"),
+        child: TextButton(
+          onPressed: () {
+            print("hello, world");
+          },
+          child: Text(
+            "Click me",
+            style: TextStyle(
+              color: ColorPallete.mainBrightYellow,
+              fontSize: 24,
+            ),
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -24,8 +34,9 @@ class Home extends StatelessWidget {
         child: Icon(
           Icons.add,
           size: 30.0,
+          color: ColorPallete.greySecondary,
         ),
-        backgroundColor: Colors.yellow[900],
+        backgroundColor: ColorPallete.semiDarkPrimary,
       ),
     );
   }
